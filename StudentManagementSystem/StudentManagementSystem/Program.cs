6379b0ee2db81dcba301b28547b3a608a3b22e40
mohamed3reda2 <mohamedreda.j.6@gmail.com>
2025-07-13 23:26:22 +0300

    ADD Account Controll V2

--- StudentManagementSystem/StudentManagementSystem/Program.cs
+++ StudentManagementSystem/StudentManagementSystem/Program.cs
@@ -1,117 +1,79 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using StudentManagementSystem.Data;
-using StudentManagementSystem.Service.Implementation;
 using StudentManagementSystem.Service.Interface;
-using StudentManagementSystem.Service;
+using StudentManagementSystem.Service.Implementation;
 
-namespace StudentManagementSystem
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllersWithViews();
+
+// Add Entity Framework
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+        options.SlidingExpiration = true;
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+    });
+
+// Add Authorization
+builder.Services.AddAuthorization();
+
+// Register all services
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IFieldService, FieldService>();
+builder.Services.AddScoped<IAcademicYearService, AcademicYearService>();
+builder.Services.AddScoped<IClassService, ClassService>();
+builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ICompetenceService, CompetenceService>();
+builder.Services.AddScoped<IOutcomeService, OutcomeService>();
+builder.Services.AddScoped<ITaskEvaluationService, TaskEvaluationService>();
+builder.Services.AddScoped<IPictureService, PictureService>();
+builder.Services.AddScoped<IAttendanceService, AttendanceService>();
+builder.Services.AddScoped<IAbsenceReasonService, AbsenceReasonService>();
+builder.Services.AddScoped<IStudentManagementService, StudentManagementService>();
+// Add Session (optional)
+builder.Services.AddSession(options =>
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+var app = builder.Build();
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add Entity Framework
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-
-            // Register all services
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IRoleService, RoleService>();
-            services.AddScoped<IFieldService, FieldService>();
-            services.AddScoped<IAcademicYearService, AcademicYearService>();
-            services.AddScoped<IClassService, ClassService>();
-            services.AddScoped<IStudentService, StudentService>();
-            services.AddScoped<ICompetenceService, CompetenceService>();
-            services.AddScoped<IOutcomeService, OutcomeService>();
-            services.AddScoped<ITaskEvaluationService, TaskEvaluationService>();
-            services.AddScoped<IPictureService, PictureService>();
-            services.AddScoped<IAttendanceService, AttendanceService>();
-            services.AddScoped<IAbsenceReasonService, AbsenceReasonService>();
-            services.AddScoped<IStudentManagementService, StudentManagementService>();
-
-            // Add MVC
-            services.AddControllersWithViews();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
-
-    // For .NET 6+ Program.cs approach
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add Entity Framework
-            builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-            // Register all services
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IRoleService, RoleService>();
-            builder.Services.AddScoped<IFieldService, FieldService>();
-            builder.Services.AddScoped<IAcademicYearService, AcademicYearService>();
-            builder.Services.AddScoped<IClassService, ClassService>();
-            builder.Services.AddScoped<IStudentService, StudentService>();
-            builder.Services.AddScoped<ICompetenceService, CompetenceService>();
-            builder.Services.AddScoped<IOutcomeService, OutcomeService>();
-            builder.Services.AddScoped<ITaskEvaluationService, TaskEvaluationService>();
-            builder.Services.AddScoped<IPictureService, PictureService>();
-            builder.Services.AddScoped<IAttendanceService, AttendanceService>();
-            builder.Services.AddScoped<IAbsenceReasonService, AbsenceReasonService>();
-            builder.Services.AddScoped<IStudentManagementService, StudentManagementService>();
-
-            // Add services to the container
-            builder.Services.AddControllersWithViews();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
-        }
-    }
+// Configure the HTTP request pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Authentication & Authorization middleware
+app.UseAuthentication();
+app.UseAuthorization();
+
+// Session middleware (optional)
+app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
