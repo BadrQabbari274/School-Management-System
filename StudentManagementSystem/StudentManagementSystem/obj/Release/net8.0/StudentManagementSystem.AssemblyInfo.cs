beb96a329c9b7b32dc899bb7219d4830f3452a8f
sd <sd@COM171-LAB3>
2025-07-16 15:07:36 +0300

    after add student controller

--- StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89c5e28e7656d8c50f3a9edb783cf438404a3c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
