2aed101107f49b63d561fbbdb265a4e4cf672152
sd <sd@COM175-LAB9>
2025-07-21 14:55:26 +0300

    alter field

--- StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c11e749bbd447f485e79a2741f2f9c461f70461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9069ca106c21e999dcf89ea314f11f943429293")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
