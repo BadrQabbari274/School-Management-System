bfeebafdd1a4f62adcba78114de7926f27cedad4
Ahmed40404 <Ahmed21hossam08@gmail.com>
2025-07-27 11:03:25 +0300

    filed local

--- StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69490fb73f381f3ebb2d5557a76116b8e205087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8627c52f5165ccea1a64f441368ad164f468fc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
