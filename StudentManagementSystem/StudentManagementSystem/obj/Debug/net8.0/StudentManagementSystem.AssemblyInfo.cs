1328f3bd3d7d782558fb84d4b3e4ef1c8708d013
sd <sd@COM169-LAB3>
2025-07-29 15:05:45 +0300

    LastUpdate29

--- StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca70fda54e6360069313cb22d7c35d97572656d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e38077c65ecadbed296ab433f2eae57a1f27f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
