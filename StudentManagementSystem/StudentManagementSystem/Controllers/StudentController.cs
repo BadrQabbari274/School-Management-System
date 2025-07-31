a36ce845dcf56ed981f3bdf02dab2b5a902aa975
sd <sd@COM171-LAB3>
2025-07-31 12:24:42 +0300

    after add AssignClassToStudent in Controller

--- StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/StudentManagementSystem/Controllers/StudentController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using StudentManagementSystem.Models;
 using StudentManagementSystem.Service.Interface;
+using StudentManagementSystem.ViewModels;
 using System.IO;
 
 namespace StudentManagementSystem.Controllers
@@ -12,14 +13,16 @@ namespace StudentManagementSystem.Controllers
     {
         private readonly IStudentService _studentService;
         private readonly IGradeService _gradeService;
+        private readonly IClassService _classService;
         private readonly ISectionService _sectionService;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public StudentController(IStudentService studentService, ISectionService sectionService, IGradeService gradeService, IWebHostEnvironment webHostEnvironment)
+        public StudentController(IStudentService studentService, ISectionService sectionService, IGradeService gradeService,IClassService classService, IWebHostEnvironment webHostEnvironment)
         {
             _studentService = studentService;
            _sectionService = sectionService;
             _gradeService = gradeService;
+            _classService = classService;
             _webHostEnvironment = webHostEnvironment;
         }
 
@@ -316,6 +319,22 @@ namespace StudentManagementSystem.Controllers
                 await PopulateViewBag();
                 return View(student);
             }
+        }
+
+        public async Task<IActionResult> AssignClassToStudentAsync(int Id) {
+          Classes Class = await _classService.GetClassByIdAsync(Id);
+            if (Class.Grade.Name.ToLower() == "Junior".ToLower())
+            {
+                var SectionWithStudent = _studentService.GetStudentsGroupedBySectionAsync();
+                return View(SectionWithStudent);
+            }
+            else
+            {
+                var ClassWithStudent = _studentService.GetStudentsGroupedByClassAsync(Class.Grade.Id);
+                return View(ClassWithStudent);
+                
+            }
+   
         }
 
         // POST: Student/DeleteConfirmed
--- StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b85c613ad6159c360d50849b6223f608a0fac3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2094858bedc9db30a2c3e5355cf249db42320a76")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
