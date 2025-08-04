3ddb0586389749e3bac33324d8bfe6acdc079314
yassin <sd@COM172-LAB3>
2025-08-04 10:05:36 +0200

    pull

--- StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Debug/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f740ce176a161cd6122e544fa66c495061a9950")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cfc1295ca550d11e656bd06e975c9c4f84b8b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
