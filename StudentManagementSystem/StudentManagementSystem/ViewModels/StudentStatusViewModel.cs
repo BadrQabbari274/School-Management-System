eb1eb113f7c899caec3b8debe3079ac29d33187b
sd <sd@COM171-LAB3>
2025-08-04 12:59:55 +0300

    add Attendance View Model

---
+++ StudentManagementSystem/StudentManagementSystem/ViewModels/StudentStatusViewModel.cs
@@ -0,0 +1,11 @@
+﻿using StudentManagementSystem.ViewModels;
+using StudentManagementSystem.Models;
+
+namespace StudentManagementSystem.ViewModels
+{
+    public class StudentStatusViewModel
+    {
+        public Students Students { get; set; } 
+        public bool Status  { get; set; } =true;
+    }
+}
