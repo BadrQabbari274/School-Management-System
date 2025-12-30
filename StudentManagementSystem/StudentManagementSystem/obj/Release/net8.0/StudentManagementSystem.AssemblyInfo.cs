e56ee4a8b4cbbe53629c38cf873a35a0777a853a
Badr Eldin <BadrQabbari274.github.com>
2025-12-30 20:12:58 +0200

    first commit

--- StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
+++ StudentManagementSystem/StudentManagementSystem/obj/Release/net8.0/StudentManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1910a70db395496fe763693c1c0869c5b7a28674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a373a7943392795df36c7208373fbdef149a1f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
